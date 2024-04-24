faf2bfb5fe3ae667dd756bf31d4da29322759cbc
kakzw <kento.akazawa06@gmail.com>
2024-04-24 16:50:44 -0400

    initial commit

---
+++ Chatbot/ChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotApp.swift
+//  Chatbot
+//
+//  Created by Kento Akazawa on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatbotApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
